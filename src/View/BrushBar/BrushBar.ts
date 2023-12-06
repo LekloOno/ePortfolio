df05af0bc10153da68394935e1777ad64ea3b30a
Leklo <leklo.sounds@gmail.com>
2023-12-06 20:49:48 +0100

    BrushBar Restruct - Integrated RadiusControl to Brush init

--- src/View/BrushBar/BrushBar.ts
+++ src/View/BrushBar/BrushBar.ts
@@ -1,18 +1,22 @@
 import { MassControl } from "./MassControl";
+import { RadiusControl } from "./RadiusControl";
 
 export class BrushBar {
     brushBar: HTMLDivElement;
     private _massControl: MassControl;
+    private _radiusControl: RadiusControl;
 
     constructor(initMass = 100000, maxMass = 1000000000000){
         this.brushBar = document.createElement("div");
         this.brushBar.id = "sandBoxBrush";
 
         this._massControl = new MassControl();
+        this._radiusControl = new RadiusControl();
         this.buildBrushBar();
     }
 
     buildBrushBar(){
         this.brushBar.append(this._massControl.massControl);
+        this.brushBar.append(this._radiusControl.radiusControl);
     }
 }
