2eab8b1ba46fc85c7c11ef95d6dfabeba89fccc6
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2023-12-07 15:54:19 +0100

    Bug fix - ColorHelp label wasn't appended

--- dest/View/BrushBar/ColorHelp.js
+++ dest/View/BrushBar/ColorHelp.js
@@ -4,15 +4,18 @@ export class ColorHelp {
     }
     constructor(game) {
         this._game = game;
-        this._colorHelp = document.createElement("input");
+        this._colorHelp = document.createElement("div");
         this._colorHelp.id = "colorHelp";
-        this._colorHelp.type = "checkbox";
+        this._colorHelpInput = document.createElement("input");
+        this._colorHelpInput.type = "checkbox";
         this._colorHelpLabel = document.createElement("label");
         this._colorHelpLabel.textContent = "Color Help ";
-        this._colorHelp.addEventListener("change", (event) => {
+        this._colorHelpInput.addEventListener("change", (event) => {
             this._game.switchColorHelp();
             if (!this._game.isRunning)
                 game.draw();
         });
+        this._colorHelp.appendChild(this._colorHelpInput);
+        this._colorHelp.appendChild(this._colorHelpLabel);
     }
 }
--- src/View/BrushBar/ColorHelp.ts
+++ src/View/BrushBar/ColorHelp.ts
@@ -1,7 +1,8 @@
 import { Game } from "../../Model/Game.js";
 
 export class ColorHelp {
-    private _colorHelp: HTMLInputElement;
+    private _colorHelp: HTMLDivElement;
+    private _colorHelpInput: HTMLInputElement;
     private _colorHelpLabel: HTMLLabelElement;
     private _game: Game;
 
@@ -12,17 +13,22 @@ export class ColorHelp {
     constructor(game: Game) {        
         this._game = game;
 
-        this._colorHelp = document.createElement("input");
-        this._colorHelp.id = "colorHelp";
-        this._colorHelp.type = "checkbox";
+        this._colorHelp = document.createElement("div");
+        this._colorHelp.id = "colorHelp"
+
+        this._colorHelpInput = document.createElement("input");
+        this._colorHelpInput.type = "checkbox";
 
         this._colorHelpLabel = document.createElement("label");
         this._colorHelpLabel.textContent = "Color Help ";
         
-        this._colorHelp.addEventListener("change", (event) => {
+        this._colorHelpInput.addEventListener("change", (event) => {
             this._game.switchColorHelp();
             if(!this._game.isRunning) game.draw();
         });
+
+        this._colorHelp.appendChild(this._colorHelpInput);
+        this._colorHelp.appendChild(this._colorHelpLabel);
     }
 
 }
