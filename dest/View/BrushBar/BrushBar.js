920dd1748d8a08ba7806602e1906f3a7e81ec008
LekloOno <shems.petremand.etu@univ-lille.fr>
2023-12-08 14:26:35 +0100

    Dirty Helper - should be in an independant module

--- dest/View/BrushBar/BrushBar.js
+++ dest/View/BrushBar/BrushBar.js
@@ -12,6 +12,11 @@ export class BrushBar {
         return this._radiusControl.radius;
     }
     constructor(game) {
+        this._shortcutsHelp = document.createElement("div");
+        this._shortcutsHelp.id = "shortcutsHelp";
+        this._shortcutsHelp.hidden = true;
+        this._shortcutMore = document.createElement("button");
+        this._shortcutMore.textContent = "Show Help";
         this._brushBar = document.createElement("div");
         this._brushBar.id = "sandBoxBrush";
         this._massControl = new MassControl();
@@ -23,5 +28,35 @@ export class BrushBar {
         this._brushBar.append(this._massControl.massControl);
         this._brushBar.append(this._radiusControl.radiusControl);
         this._brushBar.append(this._colorHelp.colorHelp);
+        let createHelp = document.createElement("p");
+        let velHelp = document.createElement("p");
+        let selectHelp = document.createElement("p");
+        let deleteHelp = document.createElement("p");
+        let followHelp = document.createElement("p");
+        let pauseHelp = document.createElement("p");
+        createHelp.innerHTML = "<br>Press <b>Left Click</b> to create a celestial body";
+        selectHelp.innerHTML = "Hold <b>Left Click</b> to select bodies";
+        velHelp.innerHTML = "Hold <b>Ctrl</b> while holding <b>Left Click</b> to create a body with an initial velocity";
+        deleteHelp.innerHTML = "Press <b>Delete</b> to delete the selection";
+        followHelp.innerHTML = "Press <b>F</b> to follow the selected body (only one selection)";
+        pauseHelp.innerHTML = "Press <b>P</b> to pause the simulation";
+        this._shortcutsHelp.appendChild(createHelp);
+        this._shortcutsHelp.appendChild(velHelp);
+        this._shortcutsHelp.appendChild(selectHelp);
+        this._shortcutsHelp.appendChild(deleteHelp);
+        this._shortcutsHelp.appendChild(followHelp);
+        this._shortcutsHelp.appendChild(pauseHelp);
+        this._brushBar.appendChild(this._shortcutsHelp);
+        this._brushBar.appendChild(this._shortcutMore);
+        this._shortcutMore.addEventListener('click', () => {
+            if (this._shortcutsHelp.hidden) {
+                this._shortcutMore.textContent = 'Hide Help';
+            }
+            else {
+                this._shortcutMore.textContent = 'Show Help';
+            }
+            this._shortcutsHelp.hidden = !this._shortcutsHelp.hidden;
+            this._shortcutMore.blur();
+        });
     }
 }
--- src/View/BrushBar/BrushBar.ts
+++ src/View/BrushBar/BrushBar.ts
@@ -9,6 +9,9 @@ export class BrushBar {
     private _radiusControl: RadiusControl;
     private _colorHelp: ColorHelp;
 
+    private _shortcutMore: HTMLButtonElement;
+    private _shortcutsHelp: HTMLDivElement;
+
     get brushBar() {
         return this._brushBar;
     }
@@ -22,6 +25,12 @@ export class BrushBar {
     }
 
     constructor(game: Game) {
+        this._shortcutsHelp = document.createElement("div");
+        this._shortcutsHelp.id = "shortcutsHelp";
+        this._shortcutsHelp.hidden = true;
+
+        this._shortcutMore = document.createElement("button");
+        this._shortcutMore.textContent = "Show Help";
 
         this._brushBar = document.createElement("div");
         this._brushBar.id = "sandBoxBrush";
@@ -36,5 +45,42 @@ export class BrushBar {
         this._brushBar.append(this._massControl.massControl);
         this._brushBar.append(this._radiusControl.radiusControl);
         this._brushBar.append(this._colorHelp.colorHelp);
+
+        
+        let createHelp = document.createElement("p");
+        let velHelp = document.createElement("p");
+        let selectHelp = document.createElement("p");
+        let deleteHelp = document.createElement("p");
+        let followHelp = document.createElement("p");
+        let pauseHelp = document.createElement("p");
+
+        
+        createHelp.innerHTML = "<br>Press <b>Left Click</b> to create a celestial body";
+        selectHelp.innerHTML = "Hold <b>Left Click</b> to select bodies";
+        velHelp.innerHTML = "Hold <b>Ctrl</b> while holding <b>Left Click</b> to create a body with an initial velocity";
+        deleteHelp.innerHTML = "Press <b>Delete</b> to delete the selection";
+        followHelp.innerHTML = "Press <b>F</b> to follow the selected body (only one selection)";
+        pauseHelp.innerHTML = "Press <b>P</b> to pause the simulation";
+        
+        this._shortcutsHelp.appendChild(createHelp);
+        this._shortcutsHelp.appendChild(velHelp);
+        this._shortcutsHelp.appendChild(selectHelp);
+        this._shortcutsHelp.appendChild(deleteHelp);
+        this._shortcutsHelp.appendChild(followHelp);
+        this._shortcutsHelp.appendChild(pauseHelp);
+
+        this._brushBar.appendChild(this._shortcutsHelp);
+
+        this._brushBar.appendChild(this._shortcutMore);
+        this._shortcutMore.addEventListener('click', () => {
+            if(this._shortcutsHelp.hidden){
+                this._shortcutMore.textContent = 'Hide Help';
+            } else {
+                this._shortcutMore.textContent = 'Show Help';
+            }
+
+            this._shortcutsHelp.hidden = !this._shortcutsHelp.hidden;
+            this._shortcutMore.blur();
+        })
     }
 }
