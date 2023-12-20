68e54e19f1500842e55afd6b556a89b353ea1fe0
Shems Petremand <shems.petremand.etu@univ-lille.fr>
2023-12-20 18:39:29 +0100

    Hide portfolio html setup

---
+++ src/ePortfolio/menu.ts
@@ -0,0 +1,13 @@
+const elements = document.getElementsByClassName("portfolio");
+var sandboxMode = false;
+
+addEventListener("keypress", (event) => {
+    if(event.key == "m") {
+       Array.prototype.forEach.call(elements, (element) => {
+        element.hidden = !sandboxMode;
+       });
+       sandboxMode = !sandboxMode;
+    }
+})
+
+//Presentation Formations compétences expériences projets contacts
