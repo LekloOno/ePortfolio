dc8c7d4ada0de38101436e2e8bd104d2f80e3703
Leklo <leklo.sounds@gmail.com>
2023-12-21 03:04:44 +0100

    Mouse interraction on shader

--- ShaderController.js
+++ ShaderController.js
@@ -17,11 +17,23 @@ var targetPos = new Vector2(0, 0);
 var prevTargetPos = new Vector2(0, 0);
 var vel = new Vector2(0, 0);
 
+var targetMouse = new Vector2(0, 0);
+var mouse = new Vector2(0, 0);
+
 var maxSpeed = 40;
 
 function lerp(a, b, f){
   return a + f*(b-a);
 }
+
+function lerpv(a, b, f){
+  return new Vector2(lerp(a.x, b.x, f), lerp(a.y, b.y, f));
+}
+
+document.addEventListener("mousemove", (event) => {
+  targetMouse.x = event.clientX;
+  targetMouse.y = event.clientY;
+});
 
 shaderWebBackground.shade({
 
@@ -59,12 +71,18 @@ shaderWebBackground.shade({
     pos.x += vel.x;
     pos.y += vel.y;
 
-    realPos.x = lerp(realPos.x, pos.x, 0.1);
-    realPos.y = lerp(realPos.y, pos.y, 0.1);
+    realPos = lerpv(realPos, pos, 0.1);
+    //realPos.x = lerp(realPos.x, pos.x, 0.1);
+    //realPos.y = lerp(realPos.y, pos.y, 0.1);
 
     console.log(pos.x);
     ctx.posX = ctx.toShaderX(realPos.x);
     ctx.posY = ctx.toShaderY(realPos.y);
+
+
+    mouse = lerpv(mouse, targetMouse, 0.005);
+    ctx.shaderMouseX = ctx.toShaderX(mouse.x);
+    ctx.shaderMouseY = ctx.toShaderY(mouse.y);
   },
   shaders: {
     image: {
@@ -72,6 +90,7 @@ shaderWebBackground.shade({
         iTime: (gl, loc) => gl.uniform1f(loc, performance.now() / 1000),
         iResolution: (gl, loc, ctx) => gl.uniform2f(loc, ctx.width, ctx.height),
         iPos: (gl, loc, ctx) => gl.uniform2f(loc, ctx.posX, ctx.posY),
+        iMouse: (gl, loc, ctx) => gl.uniform2f(loc, ctx.shaderMouseX, ctx.shaderMouseY),
       }
     }
   },
